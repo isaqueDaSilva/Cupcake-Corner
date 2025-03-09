5649f4cb999e8b6fbebec4c6015624bd67cf3481
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-03-09 18:57:14 -0300

    Initial Commit

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 3/9/25.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
