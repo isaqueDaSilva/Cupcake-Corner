987ca9ab66fe73214e896af8f4e42fcadc5eea5c
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-04-09 18:54:28 -0300

    Update - CoverImageView - Adding a documentation and refactoring the initializer to reduce the amount of the task that it's make when it construct this view, to help improving the performance of the app.

--- Common/Core/Components/CoverImageView.swift
+++ Common/Core/Components/CoverImageView.swift
@@ -7,27 +7,45 @@
 
 import SwiftUI
 
+/// Sets a default component to display the cupcake's cover image.
 struct CoverImageView: View {
-    var coverImage: Image
+    @State private var coverImage: Image?
     
-    let width: CGFloat
-    let height: CGFloat
+    /// The data representation of the image.
+    let imageData: Data?
+    
+    /// The size that we want that the image will be displayed.
+    let size: CGSize
     
     var body: some View {
-        coverImage
-            .resizable()
-            .scaledToFit()
-            .frame(width: width, height: height)
-            .clipShape(RoundedRectangle(cornerRadius: 10))
+        Group {
+            if let coverImage {
+                coverImage
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            } else {
+                Icon.questionmarkDiamond.systemImage
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+            }
+        }
+        .frame(width: size.width, height: size.height)
+        .clipShape(RoundedRectangle(cornerRadius: 10))
+        .onAppear {
+            // Checks if already had an image defined at the `coverImage` property.
+            guard coverImage == nil else { return }
+            
+            self.coverImage = .init(
+                by: self.imageData,
+                with: self.size,
+                defaultIcon: .squareSlash
+            )
+        }
     }
     
-    init(
-        imageData: Data? = nil,
-        size: CGSize
-    ) {
-        self.coverImage = .init(by: imageData, with: size, defaultIcon: .squareSlash)
-        self.width = size.width
-        self.height = size.height
+    init(imageData: Data? = nil, size: CGSize) {
+        self.imageData = imageData
+        self.size = size
     }
 }
 
