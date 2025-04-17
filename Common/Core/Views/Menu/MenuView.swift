a1741ac211803a5dd6de8ca8f74178af2b1efce1
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-04-16 21:15:09 -0300

    Updating MenuView In this update, I turning this view as base view to store only code that is run in both Client and Admin targets.

--- Common/Core/Views/Menu/MenuView.swift
+++ Common/Core/Views/Menu/MenuView.swift
@@ -9,125 +9,42 @@ import ErrorWrapper
 import SwiftUI
 
 struct MenuView: View {
-    @Environment(UserRepository.self) private var userRepository
-    @State private var viewModel: ViewModel
-    private let colums: [GridItem] = [.init(.adaptive(minimum: 150))]
+    @Bindable var cupcakeRepository: CupcakeRepository
+    @State private var viewModel = ViewModel()
     
     var body: some View {
-        NavigationStack {
-            ScrollView {
-                Group {
-                    switch viewModel.isLoading {
-                    case true:
-                        ProgressView()
-                            .containerRelativeFrame(.vertical)
-                    case false:
-                        switch viewModel.isCupcakeListEmpty {
-                        case true:
-                            EmptyStateView(
-                                title: "No Cupcake Load",
-                                description: "There are no cupcakes to be displayed.",
-                                icon: .magnifyingglass
-                            )
-                            .containerRelativeFrame(.vertical)
-                        case false:
-                            cupcakeViewLoad
-                        }
+        MenuList(cupcakeList: cupcakeRepository.cupcakes)
+            .onAppear {
+                if cupcakeRepository.isCupcakeListEmpty {
+                    viewModel.fetch { cupcakeList in
+                        cupcakeRepository.fillStorage(
+                            with: cupcakeList.cupcakes
+                        )
                     }
                 }
-                .navigationTitle("Menu")
-                #if ADMIN
-                .toolbar {
-                    Button {
-                        viewModel.isShowingCreateNewCupcake = true
-                    } label: {
-                        Icon.plusCircle.systemImage
-                    }
-                    #if !DEBUG
-                    .disabled(userRepository.user == nil)
-                    #endif
-                }
-                .sheet(isPresented: $viewModel.isShowingCreateNewCupcake) {
-                    CreateNewCupcakeView { newCupcake in
-                        viewModel.updateStorage(with: .create(newCupcake))
-                    }
-                }
-                #endif
-                .errorAlert(error: $viewModel.error) { }
-                .navigationDestination(for: Cupcake.self) { cupcake in
-                    
-                    #if CLIENT
-                    OrderView(cupcake: cupcake)
-                    #elseif ADMIN
-                    CupcakeDetailView(cupcake: cupcake) { action in
-                        viewModel.updateStorage(with: action)
-                    }
-                    #endif
-                }
-                .environment(userRepository)
             }
+            .opacity(viewModel.isLoading ? 0 : 1)
+            .overlay {
+                OverlayView(
+                    isLoading: viewModel.isLoading,
+                    isCupcakeListEmpty: cupcakeRepository.isCupcakeListEmpty
+                )
+            }
+            .navigationTitle("Menu")
+            .errorAlert(error: $viewModel.error) { }
             .refreshable {
-                viewModel.fetch()
-            }
-        }
-    }
-    
-    init(isPreview: Bool = false) {
-        self._viewModel = .init(initialValue: .init(isPreview: isPreview))
-    }
-}
-
-// MARK: - Main View -
-extension MenuView {
-    @ViewBuilder
-    private var cupcakeViewLoad: some View {
-        VStack {
-            cupcakeScrollList
-        }
-        .padding()
-    }
-}
-
-extension MenuView {
-    @ViewBuilder
-    private func CupcakeCard(
-        with flavor: String,
-        and coverImageData: Data
-    ) -> some View {
-        GroupBox {
-            ImageResizer(imageData: coverImageData, size: .midSizePicture) { image in
-                image
-                    .resizable()
-                    .scaledToFit()
-            }
-        } label: {
-            Text(flavor)
-                .lineLimit(1)
-        }
-    }
-}
-
-extension MenuView {
-    @ViewBuilder
-    private var cupcakeScrollList: some View {
-        LazyVGrid(columns: colums) {
-            ForEach(
-                viewModel.cupcakes,
-                id: \.id
-            ) { cupcake in
-                NavigationLink(value: cupcake) {
-                    CupcakeCard(
-                        with: cupcake.flavor,
-                        and: cupcake.coverImage
+                viewModel.fetch { cupcakeList in
+                    self.cupcakeRepository.fillStorage(
+                        with: cupcakeList.cupcakes
                     )
                 }
-                .buttonStyle(.plain)
             }
-        }
+            .disabled(viewModel.isLoading)
     }
 }
 
 #Preview {
-    MenuView(isPreview: true)
+    MenuView(cupcakeRepository: .init(isPreview: true))
         .environment(UserRepository())
 }
+
