f49528331915d53980924269ba2dbee7120f6d98
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-06-23 14:18:25 -0300

    CreateNewCupcakeView update Updating the properies that is called to handle to handle with specific tasks, following the view model update. Also, I update the create, called when the user press the `OK` button, that now it's call a send image method from ImageHandle to handle with send image tasks.

--- CupcakeCornerForAdmin/Core/View/Create Cupcake/CreateNewCupcakeView.swift
+++ CupcakeCornerForAdmin/Core/View/Create Cupcake/CreateNewCupcakeView.swift
@@ -9,29 +9,32 @@ import ErrorWrapper
 import SwiftUI
 
 struct CreateNewCupcakeView: View {
-    var action: (Cupcake) throws -> Void
-    
     @State private var viewModel = ViewModel()
+    @State private var imageHandler = ImageHandler()
     
     var body: some View {
         EditCupcake(
-            pickerItemSelected: $viewModel.pickerItemSelected,
-            flavorName: $viewModel.flavor,
-            price: $viewModel.price,
-            ingredients: $viewModel.ingredients,
+            pickerItemSelected: $imageHandler.pickerItemSelected,
+            flavorName: $viewModel.newCupcake.flavor,
+            price: $viewModel.newCupcake.price,
+            ingredients: $viewModel.newCupcake.ingredients,
             isLoading: $viewModel.isLoading,
             navigationTitle: "Create",
-            coverImageData: viewModel.coverImageData
+            coverImageData: imageHandler.cupcakeImage?.imageData
         ) { dismiss in
-            viewModel.create { newCupcake in
-                try action(newCupcake)
-                dismiss()
+            viewModel.create { cupcakeID, token, session in
+                try await imageHandler.sendImage(
+                    with: cupcakeID,
+                    token: token,
+                    and: session
+                )
             }
+
         }
         .errorAlert(error: $viewModel.error) { }
     }
 }
 
 #Preview {
-    CreateNewCupcakeView { _ in }
+    CreateNewCupcakeView()
 }
