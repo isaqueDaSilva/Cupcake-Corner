16f7f6afcacedb79eedb4c5f794f6da3f0a6e6a2
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-01 22:28:51 -0300

    Updating MenuListVC Updating the Table view to display a load indicator at the bottom of the list, to indicate that we fetching more items from the server.

--- Common/Core/Views/Menu/Components/MenuViewListVC.swift
+++ Common/Core/Views/Menu/Components/MenuViewListVC.swift
@@ -9,40 +9,41 @@ import UIKit
 
 final class MenuViewListVC: UITableViewController, UITableViewDataSourcePrefetching {
     private let cellIdentifier = "CardTableViewCell"
-    private let viewModel: MenuView.ViewModel
+    private let progressCellIdentifier = "ProgressViewTableViewCell"
+    private let viewModel: MenuViewModel
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        self.tableView.delegate = self
         self.fetchCupcakes()
         self.configureTableView()
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return self.viewModel.cupcakes.count
+        let cupcakesCount = viewModel.cupcakes.count
+        let sectionsCount = (viewModel.viewState == .loadedAll) ? (cupcakesCount) : (cupcakesCount + 1)
+        return (cupcakesCount > 0) ? sectionsCount : 0
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = self.tableView.dequeueReusableCell(
-            withIdentifier: self.cellIdentifier,
-            for: indexPath
-        ) as! CardTableViewCell
-        
-        let cupcake = self.viewModel.cupcakes[indexPath.row]
-        
-        cell.setElements(for: cupcake)
-        
-        return cell
+        if indexPath.row != viewModel.cupcakes.count {
+            return self.setCupcakeCell(for: indexPath)
+        } else {
+            return self.setProgressViewCell(for: indexPath)
+        }
     }
     
     func tableView(_ tableView: UITableView, prefetchRowsAt indexPaths: [IndexPath]) {
         guard viewModel.viewState == .default else { return }
         
-        print(indexPaths.count)
-        
         if (viewModel.cupcakes.last != nil) && (indexPaths.last != nil) {
             print("Start Fetching...")
             self.fetchCupcakes()
         }
+    }
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        self.viewModel.selectedCupcake = self.viewModel.cupcakes[indexPath.row]
     }
     
     @available(
@@ -55,7 +56,7 @@ final class MenuViewListVC: UITableViewController, UITableViewDataSourcePrefetch
         fatalError("init(coder:) has not been implemented")
     }
     
-    init(viewModel: MenuView.ViewModel) {
+    init(viewModel: MenuViewModel) {
         self.viewModel = viewModel
         
         super.init(style: .plain)
@@ -72,8 +73,36 @@ extension MenuViewListVC {
             forCellReuseIdentifier: self.cellIdentifier
         )
         
+        self.tableView.register(
+            ProgressViewTableViewCell.self,
+            forCellReuseIdentifier: self.progressCellIdentifier
+        )
+        
         self.tableView.prefetchDataSource = self
-        self.tableView.isPrefetchingEnabled = true
+    }
+    
+    private func setCupcakeCell(for indexPath: IndexPath) -> CardTableViewCell {
+        let cell = self.tableView.dequeueReusableCell(
+            withIdentifier: self.cellIdentifier,
+            for: indexPath
+        ) as! CardTableViewCell
+        
+        let cupcake = self.viewModel.cupcakes[indexPath.row]
+        
+        cell.setElements(for: cupcake)
+        
+        return cell
+    }
+    
+    private func setProgressViewCell(for indexPath: IndexPath) -> ProgressViewTableViewCell {
+        let cell = self.tableView.dequeueReusableCell(
+            withIdentifier: self.progressCellIdentifier,
+            for: indexPath
+        ) as! ProgressViewTableViewCell
+        
+        cell.startAnimating()
+        
+        return cell
     }
 }
 
@@ -90,7 +119,6 @@ extension MenuViewListVC {
         }
     }
 }
-
 
 #Preview {
     MenuViewListVC(viewModel: .init(isPreview: false))
