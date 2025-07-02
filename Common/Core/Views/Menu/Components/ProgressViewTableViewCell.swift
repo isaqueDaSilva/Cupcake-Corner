d217cabc7a11c0e1b5caf6077347d0922487b582
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-01 22:39:27 -0300

    Creating the ProgressViewTableViewCell This cell is used to display a progress indicator when a loading action is occuring in a table view.

--- Common/Core/Views/Menu/Components/ProgressViewTableViewCell.swift
+++ Common/Core/Views/Menu/Components/ProgressViewTableViewCell.swift
@@ -10,13 +10,61 @@ import UIKit
 class ProgressViewTableViewCell: UITableViewCell {
     private let progressView = UIActivityIndicatorView()
     
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        self.configure()
+        self.setConstraints()
+    }
+    
+    @available(
+        iOS,
+        introduced: 18,
+        deprecated: 18,
+        message: "Not utilize this initializer."
+    )
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
+
+extension ProgressViewTableViewCell {
     private func configure() {
         self.progressView.translatesAutoresizingMaskIntoConstraints = false
         self.progressView.style = .medium
         self.progressView.hidesWhenStopped = true
+        self.addSubview(self.progressView)
+    }
+    
+    private func setConstraints() {
+        NSLayoutConstraint.activate([
+            self.progressView.leadingAnchor.constraint(
+                equalTo: self.leadingAnchor
+            ),
+            self.progressView.trailingAnchor.constraint(
+                equalTo: self.trailingAnchor
+            ),
+            self.progressView.topAnchor.constraint(
+                equalTo: self.topAnchor
+            ),
+            self.progressView.bottomAnchor.constraint(
+                equalTo: self.bottomAnchor
+            )
+        ])
+    }
+}
+
+extension ProgressViewTableViewCell {
+    func startAnimating() {
+        self.progressView.startAnimating()
+    }
+    
+    func stopAnimating() {
+        self.progressView.stopAnimating()
     }
 }
 
 #Preview {
-    ProgressViewTableViewCell()
+    let progressViewCell = ProgressViewTableViewCell()
+    progressViewCell.stopAnimating()
+    return progressViewCell
 }
