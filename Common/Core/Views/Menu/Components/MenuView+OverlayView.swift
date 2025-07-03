10b6925a979d6b769559e3f4b35c90768358bf06
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-03 17:16:23 -0300

    Small updates

---
+++ Common/Core/Views/Menu/Components/MenuView+OverlayView.swift
@@ -0,0 +1,32 @@
+//
+//  OverlayView.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 4/16/25.
+//
+
+import SwiftUI
+
+extension MenuView {
+    struct OverlayView: View {
+        let isLoading: Bool
+        let isCupcakeListEmpty: Bool
+        
+        var body: some View {
+            Group {
+                switch isLoading {
+                case true:
+                    ProgressView()
+                case false:
+                    if isCupcakeListEmpty {
+                        EmptyStateView(
+                            title: "No Cupcake Load",
+                            description: "There are no cupcakes to be displayed.",
+                            icon: .magnifyingglass
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
