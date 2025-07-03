fed09907deb18b64c6149d7f1b7a8455e131c5a2
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-03 17:24:48 -0300

    CupcakeImage update Adding send image and get images to CupcakeImage model.

--- Common/Models/Cupcake/CupcakeImage.swift
+++ Common/Models/Cupcake/CupcakeImage.swift
@@ -1,3 +1,53 @@
-struct CupcakeImage: Codable {
-    let imageData: Data
+//
+//  CupcakeImage.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 6/21/25.
+//
+
+import Foundation
+
+struct CupcakeImage: Codable, Sendable {
+    var imageData: Data
+}
+
+extension CupcakeImage {
+    func sendImage(
+        with cupcakeID: String,
+        token: String,
+        session: URLSession
+    ) async throws -> Response {
+        let cupcakeImageData = try EncoderAndDecoder.encodeData(self)
+        
+        let request = _Network(
+            method: .put,
+            scheme: .https,
+            path: "/cupcake/update/\(cupcakeID)",
+            fields: [.authorization : token],
+            requestType: .upload(cupcakeImageData)
+        )
+        
+        let (_, response) = try await request.getResponse(with: session)
+        
+        return response
+    }
+    
+    static func getImage(
+        with cupcakeID: String,
+        token: String,
+        session: URLSession
+    ) async throws -> (Data, Response) {
+        let request = _Network(
+            method: .get,
+            scheme: .https,
+            path: "/cupcake/image/\(cupcakeID)",
+            fields: [
+                .authorization : token,
+                .contentType : _Network.HeaderValue.json.rawValue
+            ],
+            requestType: .get
+        )
+        
+        return try await request.getResponse(with: session)
+    }
 }
