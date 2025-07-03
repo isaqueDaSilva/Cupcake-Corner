5aa6ef23f5b80618952db48e2fa58d6657393354
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-03 17:21:23 -0300

    Creating ImageHandler This handler is responsible with get images from the user's photo library and and send to the server.

---
+++ CupcakeCornerForAdmin/Services/ImageHandler.swift
@@ -0,0 +1,77 @@
+//
+//  ImageHandler.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 3/10/25.
+//
+
+import SwiftUI
+import PhotosUI
+
+@Observable
+@MainActor
+final class ImageHandler {
+    private let logger = AppLogger(category: "ImageHandler")
+    
+    var cupcakeImage: CupcakeImage? = nil
+    var insertState: ViewState = .default
+    var pickerItemSelected: PhotosPickerItem? = nil {
+        didSet {
+            print(pickerItemSelected == nil)
+            if let pickerItemSelected {
+                self.getImage(with: pickerItemSelected)
+            }
+        }
+    }
+    
+    private func getImage(with pickerItemSelected: PhotosPickerItem) {
+        self.insertState = .loading
+        
+        pickerItemSelected.loadTransferable(type: Data.self) { [weak self] result in
+            guard let self else { return }
+            
+            switch result {
+            case .success(let data):
+                if let data {
+                    DispatchQueue.main.async { [weak self] in
+                        guard let self else { return }
+                        self.cupcakeImage = .init(imageData: data)
+                        self.insertState = .default
+                    }
+                }
+            case .failure(let error):
+                DispatchQueue.main.async { [weak self] in
+                    guard let self else { return }
+                    self.cupcakeImage = nil
+                    self.logger.error("Could not possible to load image: \(error.localizedDescription)")
+                    self.insertState = .default
+                }
+            }
+        }
+    }
+    
+    func sendImage(with cupcakeID: UUID, token: String, and session: URLSession) async throws(AppError) {
+        guard let cupcakeImage else {
+            throw AppError(
+                title: "No image",
+                descrition: "You need a image to follow with this action."
+            )
+        }
+        
+        do {
+            guard try await cupcakeImage.sendImage(
+                    with: cupcakeID.uuidString,
+                    token: token,
+                    session: session
+                ).status == .ok
+            else {
+                throw AppError.badResponse
+            }
+        } catch {
+            self.logger.error(
+                "Failed to set image for cupcake \(cupcakeID.uuidString) with error: \(error.localizedDescription)"
+            )
+            throw .init(title: "Failed to set image for this cupcake.", descrition: "")
+        }
+    }
+}
