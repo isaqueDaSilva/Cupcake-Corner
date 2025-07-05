b4131ba6013deadd957a1927e6f58b08bae1fe70
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-05 14:50:27 -0300

    Network layer update Creating a new layer to handler more easily and efficiently with network task based on swift's HTTPTypes library

--- Common/Utilities/Network/Network.swift
+++ Common/Utilities/Network/Network.swift
@@ -7,7 +7,81 @@
 
 import ErrorWrapper
 import Foundation
+import HTTPTypes
+import HTTPTypesFoundation
 import NetworkHandler
+
+typealias Response = HTTPResponse
+typealias DataAndResponse = (Data, HTTPResponse)
+
+struct _Network {
+    static let authority = "localhost:8080"
+    private let method: HTTPRequest.Method
+    private let scheme: Scheme
+    private let path: String
+    private let fields: [HTTPField.Name: String]
+    private let requestType: RequestType
+    
+    func getResponse(with urlSession: URLSession) async throws -> (Data, HTTPResponse) {
+        var request = HTTPRequest(
+            method: self.method,
+            scheme: self.scheme.rawValue,
+            authority: Self.authority,
+            path: self.path
+        )
+        
+        for (field, value) in self.fields {
+            request.headerFields.append(.init(name: field, value: value))
+        }
+        
+        return try await self.requestType.performTask(with: request, and: urlSession)
+    }
+    
+    enum RequestType {
+        case upload(Data), get
+        
+        func performTask(with request: HTTPRequest, and urlSession: URLSession) async throws -> (Data, HTTPResponse) {
+            switch self {
+            case .upload(let data):
+                try await urlSession.upload(for: request, from: data)
+            case .get:
+                try await urlSession.data(for: request)
+            }
+        }
+    }
+    
+    enum Scheme: String {
+        case https, wss
+        
+        var rawValue: String {
+            switch self {
+            case .https:
+                "https"
+            case .wss:
+                "wss"
+            }
+        }
+    }
+    
+    enum HeaderValue: String {
+        case json = "application/json"
+        case vdnAPIJSON = "application/vnd.api+json"
+    }
+    
+    init(
+        method: HTTPRequest.Method,
+        scheme: Scheme,
+        path: String,
+        fields: [HTTPField.Name : String],
+        requestType: RequestType
+    ) {
+        self.method = method
+        self.scheme = scheme
+        self.path = path
+        self.fields = fields
+        self.requestType = requestType
+    }
+}
 
 enum Network {
     static func getData(
