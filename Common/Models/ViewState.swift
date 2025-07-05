0a3ef56718c79236f4ba83d610a0754f57e25950
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-05 15:02:44 -0300

    Creating ViewState

---
+++ Common/Models/ViewState.swift
@@ -0,0 +1,10 @@
+//
+//  ViewState.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 7/5/25.
+//
+
+enum ViewState {
+    case `default`, loading, fetchingMore, refreshing, loadedAll
+}
