827f3d74991b5addd823eac5bd6b4d0dab9a55cf
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-05 14:46:55 -0300

    Creating a default ecoder and decoder for the application.

--- Common/Service/EncoderAndDecoder.swift
+++ Common/Service/EncoderAndDecoder.swift
@@ -5,4 +5,27 @@
 //  Created by Isaque da Silva on 6/21/25.
 //
 
+import ErrorWrapper
 import Foundation
+
+enum EncoderAndDecoder {
+    static func encodeData<T: Encodable>(_ model: T, encoder: JSONEncoder = .init()) throws(ExecutionError) -> Data {
+        do {
+            return try encoder.encode(model)
+        } catch {
+            throw .encodeFailure
+        }
+    }
+    
+    static func decodeResponse<T: Decodable>(
+        type: T.Type,
+        by data: Data,
+        with decoder: JSONDecoder = .init()
+    ) throws(ExecutionError) -> T {
+        guard let data = try? decoder.decode(T.self, from: data) else {
+            throw .decodedFailure
+        }
+        
+        return data
+    }
+}
