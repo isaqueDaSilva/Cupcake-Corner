4e04831da3cc259e29a40f39b2cbd9b28e6d3dfb
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-05 14:42:37 -0300

    MenuView+ViewModel update Polishing the fetch method to be a general propose method and can be used to fetching a list from scratch, refresh and also fetch more items to be added to a list as needed.

---
+++ Common/Components/Menu/MenuView+ViewModel.swift
@@ -0,0 +1,158 @@
+//
+//  MenuView+ViewModel.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 3/9/25.
+//
+
+import Foundation
+import Observation
+
+extension MenuView {
+    @Observable
+    @MainActor
+    final class ViewModel {
+        private let logger = AppLogger(category: "MenuView+ViewModel")
+        private var pageMetadata = PageMetadata()
+        
+        var cupcakes: [ReadCupcake] = []
+        var viewState: ViewState = .default
+        var error: AppError?
+        
+        var isLoading: Bool {
+            self.viewState == .loading ||
+            self.viewState == .fetchingMore ||
+            self.viewState == .refreshing
+        }
+        
+        var isCupcakeListEmpty: Bool {
+            self.cupcakes.isEmpty
+        }
+        
+        func fetchPage(session: URLSession = .shared) {
+            guard self.isCupcakeListEmpty && self.viewState == .default else { return }
+            
+            self.viewState = .loading
+            
+            Task { [weak self] in
+                guard let self else { return }
+                
+                await ImageCache.shared.removeAllImageData()
+                
+                #if DEBUG
+                await self.fetchMocks()
+                #else
+                await fetch(page: 0, session: session)
+                #endif
+            }
+        }
+        
+        func fetchMorePages(session: URLSession = .shared) {
+            guard viewState == .default else { return }
+            
+            self.viewState = .fetchingMore
+            
+            Task { [weak self] in
+                guard let self else { return }
+                
+                let nextPage = self.pageMetadata.page + 1
+                
+                #if DEBUG
+                await self.fetchMocks()
+                #else
+                await self.fetch(page: nextPage, session: session)
+                #endif
+            }
+        }
+        
+        func refresh(session: URLSession = .shared) {
+            Task {
+                await self.resetCupcakeList()
+                
+                #if DEBUG
+                await self.fetchMocks()
+                #else
+                await self.fetch(page: 0, session: session)
+                #endif
+            }
+        }
+        
+        private func fetch(
+            page: Int,
+            session: URLSession = .shared
+        ) async {
+            do {
+                let token = try TokenGetter.getValue()
+                
+                let (data, response) = try await ReadCupcake.fetch(
+                    with: token,
+                    currentPage: page,
+                    and: session
+                )
+                
+                try self.checkResponse(response)
+                
+                let cupcakesPage = try EncoderAndDecoder.decodeResponse(type: Page<ReadCupcake>.self, by: data)
+                
+                await MainActor.run {
+                    self.cupcakes += cupcakesPage.items
+                    self.pageMetadata = cupcakesPage.metadata
+                    self.viewState = self.cupcakes.count == self.pageMetadata.total ? .loadedAll : .default
+                }
+            } catch let error as AppError {
+                await self.setError(error)
+            } catch {
+                await self.setError(.init(title: "Failed to get cupcakes", descrition: error.localizedDescription))
+            }
+        }
+        
+        private func resetCupcakeList() async {
+            await ImageCache.shared.removeAllImageData()
+            
+            await MainActor.run { [weak self] in
+                guard let self else { return }
+                
+                self.cupcakes.removeAll()
+                self.pageMetadata = .init()
+                self.viewState = .refreshing
+            }
+        }
+        
+        private func checkResponse(_ response: Response) throws(AppError) {
+            guard response.status == .ok else {
+                throw .badResponse
+            }
+        }
+        
+        private func setError(_ error: AppError) async {
+            await MainActor.run { [weak self] in
+                guard let self else { return }
+                
+                self.error = error
+                self.viewState = .default
+                self.logger.error(error.descrition)
+            }
+        }
+    }
+}
+
+#if DEBUG
+extension MenuView.ViewModel {
+    private func fetchMocks() async {
+        do {
+            try await Task.sleep(for: .seconds(4))
+            
+            await MainActor.run { [weak self] in
+                guard let self else { return }
+                print("Filling...")
+                self.cupcakes += ReadCupcake.mocks
+                print("Filled with \(self.cupcakes.count) cupcakes.")
+                self.viewState = cupcakes.count < 30 ? .default : .loadedAll
+                print("View State:", viewState)
+            }
+        } catch {
+            await self.setError(.internalError)
+        }
+    }
+}
+#endif
