33d6bf0a0133cbe28123d11a25b7951db3dee9da
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-07 15:36:06 -0300

    Creating NavigationInfo

---
+++ Common/Models/NavigationInfo.swift
@@ -0,0 +1,11 @@
+//
+//  NavigationInfo.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 7/7/25.
+//
+
+struct NavigationInfo: Hashable {
+    let index: Int
+    let cupcake: ReadCupcake
+}
