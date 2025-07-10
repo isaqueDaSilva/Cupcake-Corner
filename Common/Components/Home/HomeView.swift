9309d8ba30b68103779b1dab55d46cffaad175c4
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-10 01:19:18 -0300

    Moving HomeView to a new folder

---
+++ Common/Components/Home/HomeView.swift
@@ -0,0 +1,73 @@
+//
+//  HomeView.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 3/9/25.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @Environment(UserRepository.self) private var userRepository
+    @State private var tabSelected: TabSection = .menu
+    
+    var body: some View {
+        Group {
+            switch userRepository.user {
+            case .some(_):
+                self.tabView
+                    .environment(userRepository)
+            case .none:
+                LoginView()
+            }
+        }
+        .environment(userRepository)
+    }
+}
+
+extension HomeView {
+    private var tabView: some View {
+        TabView(selection: $tabSelected) {
+            Tab(
+                TabSection.menu.title,
+                systemImage: TabSection.menu.iconName,
+                value: .menu
+            ) {
+                #if CLIENT
+                ClientMenuView()
+                #elseif ADMIN
+                AdminMenuView()
+                #endif
+            }
+            
+            Tab(
+                TabSection.orders.title,
+                systemImage: TabSection.orders.iconName,
+                value: .orders
+            ) {
+                OrderView(userRepository: userRepository)
+            }
+            
+            Tab(
+                TabSection.profile.title,
+                systemImage: TabSection.profile.iconName,
+                value: .profile
+            ) {
+                UserAccountView()
+            }
+        }
+        .tabViewStyle(.sidebarAdaptable)
+    }
+}
+
+#if DEBUG
+import SwiftData
+#Preview {
+    let inMemoryModelContext = ModelContext.inMemoryModelContext
+    let userRepository = UserRepository()
+    try? userRepository.load(with: inMemoryModelContext)
+    
+    return HomeView()
+        .environment(userRepository)
+}
+#endif
