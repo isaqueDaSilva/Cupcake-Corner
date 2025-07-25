1d65de0be9c37c02ffdcc1a549e390391194471a
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-25 12:20:19 -0300

    Create the OrderListView This List view is responsable to display the list of orders

--- Common/Components/Order/Components/OrderListView.swift
+++ Common/Components/Order/Components/OrderListView.swift
@@ -8,11 +8,49 @@
 import SwiftUI
 
 struct OrderListView: View {
+    let orders: [Order]
+    let currentViewState: ViewState
+    var paginationAction: (Bool, Int) -> Void
+    var updateAction: (Int) -> ()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            LazyVStack(spacing: 10) {
+                ForEach(orders.indices, id: \.self) { index in
+                    if !orders.isEmpty {
+                        ItemCard(
+                            imageName: orders[index].cupcakeImageName,
+                            name: orders[index].title,
+                            description: orders[index].description,
+                            price: orders[index].finalPrice
+                        )
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        .onScrollVisibilityChange(threshold: 0.8) { isVisible in
+                            self.paginationAction(isVisible, index)
+                        }
+                        #if ADMIN
+                        .contextMenu {
+                            ChangeOrderStatusButton(
+                                currentStatus: self.orders[index].status
+                            ) {
+                                self.updateAction(index)
+                            }
+                        }
+                        #endif
+                        .padding(.horizontal)
+                    }
+                }
+                
+                Group {
+                    if self.currentViewState == .fetchingMore {
+                        ProgressView()
+                    }
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    OrderListView()
+    OrderListView(orders: Order.mocks, currentViewState: .default) { _, _ in} updateAction: { _ in }
 }
