238f2ffb15cd2ac1cd1dcc95e5c967aee8803222
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-25 12:18:28 -0300

    OrderFilterPickerView update folder

---
+++ Common/Components/Order/Components/OrderListView.swift
@@ -0,0 +1,18 @@
+//
+//  OrderListView.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 7/25/25.
+//
+
+import SwiftUI
+
+struct OrderListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OrderListView()
+}
