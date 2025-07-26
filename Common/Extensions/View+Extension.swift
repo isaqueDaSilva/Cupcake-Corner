0527de3ebcb228787533018e920e9fa92fc86aac
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-26 12:57:48 -0300

    View+Extension update In this update, I create a new modifier, called errorAlert, to receive an app error type and display it to user.

--- Common/Extensions/View+Extension.swift
+++ Common/Extensions/View+Extension.swift
@@ -56,16 +56,35 @@ extension View {
     }
 }
 
-// MARK: - Selection Style -
+// MARK: - Error Alert -
 extension View {
-    func selectionStyle<Fill: ShapeStyle>(fillContent: @escaping () -> Fill) -> some View {
+    /// Defines a error alert to be used when an ``AppError`` will be thrown.
+    /// - Parameters:
+    ///   - error: A binding value to reads the current state of the error.
+    ///   - action: A ViewBuilder returning the alert’s actions to be executed when an error will thrown.
+    @ViewBuilder
+    func errorAlert(
+        error: Binding<AppError?>,
+        @ViewBuilder action: () -> some View
+    ) -> some View {
         self
-            .padding()
-            .frame(maxWidth: .infinity)
-            .background() {
-                RoundedRectangle(cornerRadius: 10.0)
-                    .stroke(lineWidth: 2.0)
-                    .fill(fillContent())
-            }
+            .alert(
+                error.wrappedValue?.title ?? "",
+                isPresented: .init(get: {
+                    // Reads the title and error description to check if one of those properties aren't nil.
+                    // If bolth are empty and the error is nil, the value returned will be false,
+                    // if bolth aren't empty and not nil the error, the value returned will be true.
+                    ((error.wrappedValue?.title.isEmpty) != nil) && ((error.wrappedValue?.description.isEmpty) != nil)
+                }, set: { _ in
+                    // When the user taps some button of the alert the error state will be setup back as nil.
+                    error.wrappedValue = nil
+                })) {
+                    action()
+                } message: {
+                    // Displays a message when the error description will be not nil.
+                    if ((error.wrappedValue?.description.isEmpty) != nil) {
+                        Text(error.wrappedValue?.description ?? "No Description Available right now.")
+                    }
+                }
     }
 }
