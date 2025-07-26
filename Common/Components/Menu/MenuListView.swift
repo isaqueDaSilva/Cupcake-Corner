50d32952e11bb37426d761ca4789a005603ef71b
Isaque da Silva <122185792+isaqueDaSilva@users.noreply.github.com>
2025-07-26 12:40:53 -0300

    Creating the MenuListView This view is responsible to display the list of cupcakes fetched from the server.

---
+++ Common/Components/Menu/MenuListView.swift
@@ -0,0 +1,51 @@
+//
+//  MenuListView.swift
+//  CupcakeCorner
+//
+//  Created by Isaque da Silva on 7/26/25.
+//
+
+import SwiftUI
+
+struct MenuListView: View {
+    let cupcakes: [ReadCupcake]
+    let currentViewState: ViewState
+    var fetchMoreAction: (Bool, Int) -> Void
+    
+    var body: some View {
+        ScrollView {
+            LazyVStack {
+                ForEach(self.cupcakes.indices, id: \.self) { index in
+                    if !self.cupcakes.isEmpty {
+                        NavigationLink(value: self.cupcakes[index]) {
+                            ItemCard(
+                                imageName: self.cupcakes[index].imageName,
+                                name: self.cupcakes[index].flavor,
+                                description: self.cupcakes[index].description,
+                                price: self.cupcakes[index].price
+                            )
+                        }
+                        .buttonStyle(.plain)
+                        .onScrollVisibilityChange(threshold: 0.8) { isVisible in
+                            self.fetchMoreAction(isVisible, index)
+                        }
+                    }
+                }
+                
+                Group {
+                    if self.currentViewState == .fetchingMore {
+                        ProgressView()
+                    }
+                }
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    MenuListView(
+        cupcakes: ReadCupcake.mocks.values.elements,
+        currentViewState: .default
+    ) { _,_ in }
+}
